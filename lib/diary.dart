a1128bc1b1f2dac088c5a8b6b6c1d4033b9bc3c7
Liza Mardiana <lizamardiana16@gmail.com>
2024-11-22 18:21:11 +0700

    revisi diary

--- lib/diary.dart
+++ lib/diary.dart
@@ -26,11 +26,9 @@ class DiaryPage extends StatefulWidget {
 class _DiaryPageState extends State<DiaryPage> {
   List<DiaryEntry> _diaryEntries = [];
   final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _tagsController = TextEditingController();
   final TextEditingController _contentController = TextEditingController();
   String _selectedMood = '';
   int? _editingIndex;
-  bool _isInputVisible = false;
 
   final List<String> _moods = [
     '😊',
@@ -52,10 +50,7 @@ class _DiaryPageState extends State<DiaryPage> {
             title: _titleController.text,
             date: DateTime.now(),
             media: [],
-            tags: _tagsController.text
-                .split(',')
-                .map((tag) => tag.trim())
-                .toList(),
+            tags: [],
             mood: _selectedMood,
             content: _contentController.text,
           ));
@@ -63,12 +58,9 @@ class _DiaryPageState extends State<DiaryPage> {
           // Update the existing entry
           _diaryEntries[_editingIndex!] = DiaryEntry(
             title: _titleController.text,
-            date: _diaryEntries[_editingIndex!].date, // Keep the original date
+            date: _diaryEntries[_editingIndex!].date,
             media: [],
-            tags: _tagsController.text
-                .split(',')
-                .map((tag) => tag.trim())
-                .toList(),
+            tags: [],
             mood: _selectedMood,
             content: _contentController.text,
           );
@@ -77,11 +69,8 @@ class _DiaryPageState extends State<DiaryPage> {
 
         // Clear input fields
         _titleController.clear();
-        _tagsController.clear();
         _contentController.clear();
         _selectedMood = '';
-        _isInputVisible =
-            false; // Hide input fields after adding/updating entry
       });
     }
   }
@@ -97,14 +86,8 @@ class _DiaryPageState extends State<DiaryPage> {
       _editingIndex = index;
       _titleController.text = _diaryEntries[index].title;
       _selectedMood = _diaryEntries[index].mood;
-      _tagsController.text = _diaryEntries[index].tags.join(', ');
       _contentController.text = _diaryEntries[index].content;
-      _isInputVisible = true; // Show input fields when in edit mode
     });
-  }
-
-  int _countWords(String text) {
-    return text.trim().isEmpty ? 0 : text.trim().split(RegExp(r'\s+')).length;
   }
 
   @override
@@ -185,12 +168,10 @@ class _DiaryPageState extends State<DiaryPage> {
                                               "Date: ${_diaryEntries[index].date.day}/${_diaryEntries[index].date.month}/${_diaryEntries[index].date.year}"),
                                           SizedBox(height: 10),
                                           Text(
-                                            "Mood: ${_diaryEntries[index].mood}",
-                                            style: TextStyle(
-                                              fontFamily: 'NotoColorEmoji',
-                                              fontSize: 24,
-                                            ),
-                                          ),
+                                              "Mood: ${_diaryEntries[index].mood}",
+                                              style: TextStyle(
+                                                  fontFamily: 'NotoColorEmoji',
+                                                  fontSize: 24)),
                                           Text(
                                               "Content: ${_diaryEntries[index].content}"),
                                         ],
@@ -216,117 +197,76 @@ class _DiaryPageState extends State<DiaryPage> {
                   );
                 },
               ),
-              if (_isInputVisible) ...[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          constraints: BoxConstraints(maxHeight: 100),
-                          child: TextField(
-                            controller: _titleController,
-                            maxLines: 3,
-                            decoration: InputDecoration(
-                              hintText: 'Add a new diary entry title',
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                            ),
-                            onChanged: (text) {
-                              if (_countWords(text) > 25) {
-                                // Limit to 25 words
-                                _titleController.text =
-                                    text.split(' ').take(25).join(' ');
-                                _titleController.selection =
-                                    TextSelection.fromPosition(TextPosition(
-                                        offset: _titleController.text.length));
-                              }
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          constraints: BoxConstraints(maxHeight: 100),
-                          child: TextField(
-                            controller: _contentController,
-                            maxLines: 3,
-                            decoration: InputDecoration(
-                              hintText: 'Write your diary content here',
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                DropdownButton<String>(
-                  value: _selectedMood.isEmpty ? null : _selectedMood,
-                  hint: Text("Choose your mood"),
-                  items: _moods.map((String mood) {
-                    return DropdownMenuItem<String>(
-                      value: mood,
-                      child: Text(
-                        mood,
-                        style: TextStyle(
-                          fontFamily: 'NotoColorEmoji',
-                          fontSize: 24,
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _selectedMood = newValue ?? '';
-                    });
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: _addOrUpdateDiaryEntry,
-                    child: Text(
-                        _editingIndex == null ? "Add Entry" : "Update Entry",
-                        style: TextStyle(color: Colors.white)),
-                    style:
-                        ElevatedButton.styleFrom(backgroundColor: Colors.pink),
-                  ),
-                ),
-              ],
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _isInputVisible = !_isInputVisible; // Toggle visibility
-            if (_isInputVisible) {
-              _editingIndex = null; // Reset editing index when showing input
-              _titleController.clear();
-              _tagsController.clear();
-              _contentController.clear();
-              _selectedMood = '';
-            }
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.pink,
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: TextField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    hintText: 'Add a new diary entry title',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: TextField(
+                  controller: _contentController,
+                  maxLines: 3,
+                  minLines: 1,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: 'Write your diary content here',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            DropdownButton<String>(
+              value: _selectedMood.isEmpty ? null : _selectedMood,
+              hint: Text("Choose your mood"),
+              items: _moods.map((String mood) {
+                return DropdownMenuItem<String>(
+                  value: mood,
+                  child: Text(
+                    mood,
+                    style: TextStyle(
+                      fontFamily: 'NotoColorEmoji',
+                      fontSize: 24,
+                    ),
+                  ),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedMood = newValue ?? '';
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.add, color: Colors.pink),
+              onPressed: _addOrUpdateDiaryEntry,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -373,13 +313,8 @@ class Logo extends StatelessWidget {
       children: [
         Image.asset('images/v1_3.png', width: 50),
         SizedBox(width: 10),
-        Text(
-          'DIARY',
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        Text('DIARY',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
       ],
     );
   }
@@ -394,11 +329,7 @@ class BackDate extends StatelessWidget {
           onTap: () {
             Navigator.pop(context);
           },
-          child: Icon(
-            Icons.arrow_back,
-            size: 30,
-            color: Colors.white,
-          ),
+          child: Icon(Icons.arrow_back, size: 30, color: Colors.white),
         ),
         SizedBox(width: 10),
       ],
