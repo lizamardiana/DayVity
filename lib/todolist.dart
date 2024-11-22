e567e15ebb23f5cfcee1e111d7963b5238f37b66
Liza Mardiana <lizamardiana16@gmail.com>
2024-11-22 13:23:57 +0700

    revisi

--- lib/todolist.dart
+++ lib/todolist.dart
@@ -25,74 +25,137 @@ class TodoListPage extends StatefulWidget {
 
 class _TodoListPageState extends State<TodoListPage> {
   List<TodoItemData> _todoItems = [];
-  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  String _selectedPriority = 'Normal';
+  DateTime? _dueDate;
+  bool _isInputVisible = false; // Variabel untuk mengontrol visibilitas input
+  int? _editingIndex; // Menyimpan index dari item yang sedang diedit
 
   void _addTodoItem() {
-    if (_controller.text.isNotEmpty) {
+    if (_titleController.text.isNotEmpty) {
       setState(() {
-        _todoItems.add(TodoItemData(title: _controller.text, isChecked: false));
-        _controller.clear();
+        if (_editingIndex != null) {
+          // Update existing item
+          _todoItems[_editingIndex!] = TodoItemData(
+            title: _titleController.text,
+            description: _descriptionController.text,
+            priority: _selectedPriority,
+            dueDate: _dueDate,
+            isChecked: false,
+          );
+          _editingIndex = null; // Reset editing index
+        } else {
+          // Add new item
+          _todoItems.add(TodoItemData(
+            title: _titleController.text,
+            description: _descriptionController.text,
+            priority: _selectedPriority,
+            dueDate: _dueDate, // Pastikan dueDate ditambahkan di sini
+            isChecked: false,
+          ));
+        }
+        _sortTodoItems(); // Panggil metode untuk mengurutkan
+        _titleController.clear();
+        _descriptionController.clear();
+        _selectedPriority = 'Normal';
+        _dueDate = null;
+        _isInputVisible = false; // Sembunyikan input setelah menambahkan item
+      });
+    }
+  }
+
+  Future<void> _selectDueDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _dueDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _dueDate) {
+      setState(() {
+        _dueDate = picked;
       });
     }
   }
 
   void _editTodoItem(int index) {
-    if (_controller.text.isNotEmpty) {
-      setState(() {
-        _todoItems[index].title = _controller.text;
-        _controller.clear();
-      });
-    }
-  }
-
-  void _deleteTodoItem(int index) {
     setState(() {
-      _todoItems.removeAt(index);
+      _editingIndex = index; // Set index untuk item yang sedang diedit
+      _titleController.text = _todoItems[index].title; // Isi title
+      _descriptionController.text =
+          _todoItems[index].description; // Isi deskripsi
+      _selectedPriority = _todoItems[index].priority; // Isi priority
+      _dueDate = _todoItems[index].dueDate; // Isi due date
+      _isInputVisible = true; // Tampilkan input
     });
   }
 
-  void _toggleCheckbox(int index, bool? value) {
-    setState(() {
-      _todoItems[index].isChecked = value!;
+  void _sortTodoItems() {
+    _todoItems.sort((a, b) {
+      // Urutkan berdasarkan prioritas
+      int priorityComparison = _comparePriority(a.priority, b.priority);
+      if (priorityComparison != 0) {
+        return priorityComparison;
+      }
+      // Jika prioritas sama, urutkan berdasarkan tanggal jatuh tempo
+      if (a.dueDate == null && b.dueDate == null) return 0;
+      if (a.dueDate == null) return 1; // a lebih rendah jika tidak ada due date
+      if (b.dueDate == null)
+        return -1; // b lebih rendah jika tidak ada due date
+      return a.dueDate!.compareTo(b.dueDate!);
     });
+  }
+
+  int _comparePriority(String priorityA, String priorityB) {
+    const priorities = ['High', 'Medium', 'Low', 'Normal'];
+    return priorities
+        .indexOf(priorityA)
+        .compareTo(priorities.indexOf(priorityB));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.pink.shade100,
-              Colors.white,
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Column(
-          children: [
-            Header(),
-            Expanded(
-              child: TodoContainer(
-                todoItems: _todoItems,
-                onDelete: _deleteTodoItem,
-                onToggle: _toggleCheckbox,
-                onEdit: _editTodoItem,
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.pink.shade100,
+                  Colors.white,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
+            child: Column(
+              children: [
+                Header(),
+                Expanded(
+                  child: TodoContainer(
+                    todoItems: _todoItems,
+                    onDelete: (index) => setState(() {
+                      _todoItems.removeAt(index);
+                    }),
+                    onToggle: (index, value) {
+                      setState(() {
+                        _todoItems[index].isChecked = value!;
+                      });
+                    },
+                    onEdit: _editTodoItem, // Pass edit function
+                  ),
+                ),
+                if (_isInputVisible) ...[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: TextField(
-                      controller: _controller,
+                      controller: _titleController,
                       decoration: InputDecoration(
-                        hintText: 'Add a new task',
+                        hintText: 'Add a new task title',
                         filled: true,
                         fillColor: Colors.white,
                         border: OutlineInputBorder(
@@ -101,16 +164,74 @@ class _TodoListPageState extends State<TodoListPage> {
                       ),
                     ),
                   ),
-                  IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: _addTodoItem,
-                    color: Colors.pink,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: _descriptionController,
+                      decoration: InputDecoration(
+                        hintText: 'Add a description',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
                   ),
+                  DropdownButton<String>(
+                    value: _selectedPriority,
+                    items: <String>['High', 'Medium', 'Low', 'Normal']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedPriority = newValue!;
+                      });
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _selectDueDate(context),
+                    child: Text('Select Due Date'),
+                  ),
+                  if (_dueDate != null)
+                    Text(
+                      'Due: ${_dueDate!.toLocal().toString().split(' ')[0]}',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  SizedBox(height: 10),
                 ],
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  _isInputVisible =
+                      true; // Tampilkan input ketika tombol tambah ditekan
+                });
+              },
+              child: Icon(Icons.add, size: 30),
+              backgroundColor: Colors.pink,
+            ),
+          ),
+          if (_isInputVisible)
+            Positioned(
+              bottom: 80,
+              right: 16,
+              child: FloatingActionButton(
+                onPressed: _addTodoItem,
+                child: Icon(Icons.check, size: 30),
+                backgroundColor: Colors.green,
               ),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
@@ -150,7 +271,7 @@ class Logo extends StatelessWidget {
         Text(
           'TO DO LIST',
           style: TextStyle(
-            fontSize: 24, // Ukuran font lebih kecil
+            fontSize: 24,
             fontWeight: FontWeight.bold,
             color: Colors.white,
           ),
@@ -165,11 +286,10 @@ class BackDate extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        // Kembali ke halaman sebelumnya
         Navigator.pop(context);
       },
       child: Icon(
-        Icons.arrow_back, // Menggunakan ikon panah kembali
+        Icons.arrow_back,
         size: 30,
         color: Colors.white,
       ),
@@ -208,10 +328,7 @@ class TodoContainer extends StatelessWidget {
               todoItem: todoItems[index],
               onDelete: () => onDelete(index),
               onToggle: (value) => onToggle(index, value),
-              onEdit: () {
-                // Set the text field with the current item title for editing
-                // This can be implemented with a dialog or another method
-              },
+              onEdit: () => onEdit(index), // Call edit function
             );
           },
         ),
@@ -222,9 +339,18 @@ class TodoContainer extends StatelessWidget {
 
 class TodoItemData {
   String title;
+  String description;
+  String priority;
+  DateTime? dueDate;
   bool isChecked;
 
-  TodoItemData({required this.title, this.isChecked = false});
+  TodoItemData({
+    required this.title,
+    this.description = '',
+    this.priority = 'Normal',
+    this.dueDate,
+    this.isChecked = false,
+  });
 }
 
 class TodoItem extends StatelessWidget {
@@ -257,13 +383,28 @@ class TodoItem extends StatelessWidget {
             onChanged: onToggle,
           ),
           Expanded(
-            child: Text(
-              todoItem.title,
-              style: TextStyle(
-                decoration:
-                    todoItem.isChecked ? TextDecoration.lineThrough : null,
-                color: Colors.black,
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  todoItem.title,
+                  style: TextStyle(
+                    decoration:
+                        todoItem.isChecked ? TextDecoration.lineThrough : null,
+                    color: Colors.black,
+                  ),
+                ),
+                if (todoItem.description.isNotEmpty)
+                  Text(
+                    todoItem.description,
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                if (todoItem.dueDate != null)
+                  Text(
+                    'Due: ${todoItem.dueDate!.toLocal().toString().split(' ')[0]}',
+                    style: TextStyle(color: Colors.red),
+                  ),
+              ],
             ),
           ),
           IconButton(
