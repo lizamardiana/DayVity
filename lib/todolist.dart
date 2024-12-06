d3a14356903bb9b7e31f8fc17093fa24482ec0b0
Liza Mardiana <lizamardiana16@gmail.com>
2024-12-07 01:48:52 +0700

    back end todolist

--- lib/todolist.dart
+++ lib/todolist.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -27,48 +31,95 @@ class _TodoListPageState extends State<TodoListPage> {
   List<TodoItemData> _todoItems = [];
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
-  String _selectedPriority = 'Normal';
   DateTime? _dueDate;
-  bool _isInputVisible = false; // Variabel untuk mengontrol visibilitas input
-  int? _editingIndex; // Menyimpan index dari item yang sedang diedit
+  bool _isInputVisible = false;
+  int? _editingIndex;
+  final String userId = 'USER_ID'; // Ganti dengan ID pengguna yang sesuai
 
-  void _addTodoItem() {
+  @override
+  void initState() {
+    super.initState();
+    _fetchTodoItems(); // Mengambil data To Do saat halaman dimuat
+  }
+
+  void _fetchTodoItems() async {
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('todoItems')
+        .where('userId', isEqualTo: userId) // Filter berdasarkan userId
+        .get();
+    setState(() {
+      _todoItems = snapshot.docs.map((doc) {
+        return TodoItemData.fromMap(doc.data() as Map<String, dynamic>, doc.id);
+      }).toList();
+    });
+  }
+
+  void _addTodoItem() async {
     if (_titleController.text.isNotEmpty) {
-      setState(() {
-        if (_editingIndex != null) {
-          // Update existing item
-          _todoItems[_editingIndex!] = TodoItemData(
-            title: _titleController.text,
-            description: _descriptionController.text,
-            priority: _selectedPriority,
-            dueDate: _dueDate,
-            isChecked: false,
-          );
-          _editingIndex = null; // Reset editing index
-        } else {
-          // Add new item
-          _todoItems.add(TodoItemData(
-            title: _titleController.text,
-            description: _descriptionController.text,
-            priority: _selectedPriority,
-            dueDate: _dueDate, // Pastikan dueDate ditambahkan di sini
-            isChecked: false,
-          ));
-        }
-        _sortTodoItems(); // Panggil metode untuk mengurutkan
-        _titleController.clear();
-        _descriptionController.clear();
-        _selectedPriority = 'Normal';
-        _dueDate = null;
-        _isInputVisible = false; // Sembunyikan input setelah menambahkan item
-      });
+      String id = _editingIndex != null
+          ? _todoItems[_editingIndex!].id
+          : FirebaseFirestore.instance.collection('todoItems').doc().id;
+
+      TodoItemData newItem = TodoItemData(
+        id: id,
+        title: _titleController.text,
+        description: _descriptionController.text,
+        dueDate: _dueDate,
+        isChecked: false,
+        userId: userId, // Menyimpan userId saat menambah item
+      );
+
+      if (_editingIndex != null) {
+        await FirebaseFirestore.instance
+            .collection('todoItems')
+            .doc(id)
+            .update(newItem.toMap());
+        setState(() {
+          _todoItems[_editingIndex!] = newItem;
+          _editingIndex = null;
+        });
+      } else {
+        await FirebaseFirestore.instance
+            .collection('todoItems')
+            .doc(id)
+            .set(newItem.toMap());
+        setState(() {
+          _todoItems.add(newItem);
+        });
+      }
+
+      _clearInputFields();
     }
   }
 
-  Future<void> _selectDueDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
+  void _clearInputFields() {
+    _titleController.clear();
+    _descriptionController.clear();
+    _dueDate = null;
+    _isInputVisible = false;
+  }
+
+  void _editTodoItem(int index) {
+    setState(() {
+      _editingIndex = index;
+      _titleController.text = _todoItems[index].title;
+      _descriptionController.text = _todoItems[index].description;
+      _dueDate = _todoItems[index].dueDate;
+      _isInputVisible = true;
+    });
+  }
+
+  void _deleteTodoItem(String id) async {
+    await FirebaseFirestore.instance.collection('todoItems').doc(id).delete();
+    setState(() {
+      _todoItems.removeWhere((item) => item.id == id);
+    });
+  }
+
+  Future<void> _selectDueDate() async {
+    DateTime? picked = await showDatePicker(
       context: context,
-      initialDate: _dueDate ?? DateTime.now(),
+      initialDate: DateTime.now(),
       firstDate: DateTime(2000),
       lastDate: DateTime(2101),
     );
@@ -77,41 +128,6 @@ class _TodoListPageState extends State<TodoListPage> {
         _dueDate = picked;
       });
     }
-  }
-
-  void _editTodoItem(int index) {
-    setState(() {
-      _editingIndex = index; // Set index untuk item yang sedang diedit
-      _titleController.text = _todoItems[index].title; // Isi title
-      _descriptionController.text =
-          _todoItems[index].description; // Isi deskripsi
-      _selectedPriority = _todoItems[index].priority; // Isi priority
-      _dueDate = _todoItems[index].dueDate; // Isi due date
-      _isInputVisible = true; // Tampilkan input
-    });
-  }
-
-  void _sortTodoItems() {
-    _todoItems.sort((a, b) {
-      // Urutkan berdasarkan prioritas
-      int priorityComparison = _comparePriority(a.priority, b.priority);
-      if (priorityComparison != 0) {
-        return priorityComparison;
-      }
-      // Jika prioritas sama, urutkan berdasarkan tanggal jatuh tempo
-      if (a.dueDate == null && b.dueDate == null) return 0;
-      if (a.dueDate == null) return 1; // a lebih rendah jika tidak ada due date
-      if (b.dueDate == null)
-        return -1; // b lebih rendah jika tidak ada due date
-      return a.dueDate!.compareTo(b.dueDate!);
-    });
-  }
-
-  int _comparePriority(String priorityA, String priorityB) {
-    const priorities = ['High', 'Medium', 'Low', 'Normal'];
-    return priorities
-        .indexOf(priorityA)
-        .compareTo(priorities.indexOf(priorityB));
   }
 
   @override
@@ -138,15 +154,18 @@ class _TodoListPageState extends State<TodoListPage> {
                 Expanded(
                   child: TodoContainer(
                     todoItems: _todoItems,
-                    onDelete: (index) => setState(() {
-                      _todoItems.removeAt(index);
-                    }),
+                    onDelete: (id) => _deleteTodoItem(id),
                     onToggle: (index, value) {
                       setState(() {
-                        _todoItems[index].isChecked = value!;
+                        _todoItems[index].isChecked =
+                            value!; // Memperbarui status
+                        FirebaseFirestore.instance
+                            .collection('todoItems')
+                            .doc(_todoItems[index].id)
+                            .update({'isChecked': value});
                       });
                     },
-                    onEdit: _editTodoItem, // Pass edit function
+                    onEdit: _editTodoItem,
                   ),
                 ),
                 if (_isInputVisible) ...[
@@ -178,23 +197,8 @@ class _TodoListPageState extends State<TodoListPage> {
                       ),
                     ),
                   ),
-                  DropdownButton<String>(
-                    value: _selectedPriority,
-                    items: <String>['High', 'Medium', 'Low', 'Normal']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selectedPriority = newValue!;
-                      });
-                    },
-                  ),
                   ElevatedButton(
-                    onPressed: () => _selectDueDate(context),
+                    onPressed: _selectDueDate,
                     child: Text('Select Due Date'),
                   ),
                   if (_dueDate != null)
@@ -202,6 +206,20 @@ class _TodoListPageState extends State<TodoListPage> {
                       'Due: ${_dueDate!.toLocal().toString().split(' ')[0]}',
                       style: TextStyle(color: Colors.red),
                     ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: _addTodoItem,
+                      child: Text('Simpan'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pink,
+                        textStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
                   SizedBox(height: 10),
                 ],
               ],
@@ -213,24 +231,16 @@ class _TodoListPageState extends State<TodoListPage> {
             child: FloatingActionButton(
               onPressed: () {
                 setState(() {
-                  _isInputVisible =
-                      true; // Tampilkan input ketika tombol tambah ditekan
+                  _isInputVisible = !_isInputVisible;
+                  if (!_isInputVisible) {
+                    _clearInputFields();
+                  }
                 });
               },
               child: Icon(Icons.add, size: 30),
               backgroundColor: Colors.pink,
             ),
           ),
-          if (_isInputVisible)
-            Positioned(
-              bottom: 80,
-              right: 16,
-              child: FloatingActionButton(
-                onPressed: _addTodoItem,
-                child: Icon(Icons.check, size: 30),
-                backgroundColor: Colors.green,
-              ),
-            ),
         ],
       ),
     );
@@ -250,48 +260,13 @@ class Header extends StatelessWidget {
           ],
         ),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Logo(),
-          BackDate(),
-        ],
-      ),
-    );
-  }
-}
-
-class Logo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image.asset('images/v7_116.png', width: 100),
-        SizedBox(width: 10),
-        Text(
-          'TO DO LIST',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
+      child: Text(
+        'TO DO LIST',
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
         ),
-      ],
-    );
-  }
-}
-
-class BackDate extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pop(context);
-      },
-      child: Icon(
-        Icons.arrow_back,
-        size: 30,
-        color: Colors.white,
       ),
     );
   }
@@ -299,7 +274,7 @@ class BackDate extends StatelessWidget {
 
 class TodoContainer extends StatelessWidget {
   final List<TodoItemData> todoItems;
-  final Function(int) onDelete;
+  final Function(String) onDelete;
   final Function(int, bool?) onToggle;
   final Function(int) onEdit;
 
@@ -313,110 +288,100 @@ class TodoContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 0.95 * MediaQuery.of(context).size.width,
-      height: 0.8 * MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.8),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListView.builder(
-          itemCount: todoItems.length,
-          itemBuilder: (context, index) {
-            return TodoItem(
-              todoItem: todoItems[index],
-              onDelete: () => onDelete(index),
-              onToggle: (value) => onToggle(index, value),
-              onEdit: () => onEdit(index), // Call edit function
-            );
-          },
-        ),
+      width: MediaQuery.of(context).size.width * 0.95,
+      height: MediaQuery.of(context).size.height * 0.8,
+      child: ListView.builder(
+        itemCount: todoItems.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Checkbox(
+                value: todoItems[index].isChecked,
+                onChanged: (bool? value) {
+                  onToggle(index, value);
+                },
+              ),
+              title: Text(
+                todoItems[index].title,
+                style: TextStyle(
+                  decoration: todoItems[index].isChecked
+                      ? TextDecoration.lineThrough
+                      : null,
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(todoItems[index].description),
+                  if (todoItems[index].dueDate != null)
+                    Text(
+                      'Due: ${todoItems[index].dueDate!.toLocal().toString().split(' ')[0]}',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                ],
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      onEdit(index);
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      onDelete(todoItems[index].id);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
 class TodoItemData {
+  String id;
   String title;
   String description;
-  String priority;
   DateTime? dueDate;
-  bool isChecked;
+  bool isChecked; // Tidak lagi final
+  String userId;
 
   TodoItemData({
+    required this.id,
     required this.title,
-    this.description = '',
-    this.priority = 'Normal',
-    this.dueDate,
+    required this.description,
+    required this.dueDate,
     this.isChecked = false,
-  });
-}
-
-class TodoItem extends StatelessWidget {
-  final TodoItemData todoItem;
-  final VoidCallback onDelete;
-  final ValueChanged<bool?> onToggle;
-  final VoidCallback onEdit;
-
-  TodoItem({
-    required this.todoItem,
-    required this.onDelete,
-    required this.onToggle,
-    required this.onEdit,
+    required this.userId,
   });
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 20),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.pink.shade200,
-        borderRadius: BorderRadius.circular(5),
-        border: Border(bottom: BorderSide(color: Colors.pink.shade100)),
-      ),
-      child: Row(
-        children: [
-          Checkbox(
-            value: todoItem.isChecked,
-            onChanged: onToggle,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  todoItem.title,
-                  style: TextStyle(
-                    decoration:
-                        todoItem.isChecked ? TextDecoration.lineThrough : null,
-                    color: Colors.black,
-                  ),
-                ),
-                if (todoItem.description.isNotEmpty)
-                  Text(
-                    todoItem.description,
-                    style: TextStyle(color: Colors.black54),
-                  ),
-                if (todoItem.dueDate != null)
-                  Text(
-                    'Due: ${todoItem.dueDate!.toLocal().toString().split(' ')[0]}',
-                    style: TextStyle(color: Colors.red),
-                  ),
-              ],
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.edit, color: Colors.black),
-            onPressed: onEdit,
-          ),
-          IconButton(
-            icon: Icon(Icons.delete, color: Colors.red),
-            onPressed: onDelete,
-          ),
-        ],
-      ),
+  factory TodoItemData.fromMap(Map<String, dynamic> map, String id) {
+    return TodoItemData(
+      id: id,
+      title: map['title'],
+      description: map['description'],
+      dueDate: map['dueDate'] != null
+          ? (map['dueDate'] as Timestamp).toDate()
+          : null,
+      isChecked: map['isChecked'] ?? false,
+      userId: map['userId'],
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'description': description,
+      'dueDate': dueDate != null ? Timestamp.fromDate(dueDate!) : null,
+      'isChecked': isChecked,
+      'userId': userId,
+    };
   }
 }
